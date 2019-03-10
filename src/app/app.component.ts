942107c786bf199dcf8d812df7d924a6e3c170cc
Mayank Nagar 2 <maynagar@publicisgroupe.net>
2019-03-10 19:58:22 +0530

    Trade data updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ServerServices } from './services/server.services';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'metallica';
+
+export class AppComponent implements OnInit {
+  public tradedata : JSON;
+  public data : any[];
+  //tradeServerService : ServerServices; 
+  title = 'Metallica';
+  constructor(private serverService: ServerServices){
+
+  this.serverService.getTradeServerData()
+      .subscribe(
+        (data:JSON)=> this.tradedata = data,
+        (error)=> console.log(error),
+        ()=> {   //Runs on Completion
+          //console.log(this.tradedata);  
+          console.log('start');         
+          this.data = this.tradedata["tradedata"];          
+         console.log(this.tradedata["tradedata"][0]); 
+          console.log('end');         
+          
+        }  
+      );
+      
+  }
+  
+  ngOnInit():void{
+    //this.data = this.tradedata;
+  }
 }
